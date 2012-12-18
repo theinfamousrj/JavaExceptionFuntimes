b6a53451a7f68a76d99ef99964e5dc1a54ca798b
theinfamousrj <infamous.rj@gmail.com>
2012-12-18 13:45:35 -0500

    tested the email, working.

--- src/com/rjh/javaexception/Mailer.java
+++ src/com/rjh/javaexception/Mailer.java
@@ -1,9 +1,45 @@
 package com.rjh.javaexception;
 
+import org.apache.commons.mail.*;
+
 public class Mailer {
 
+	//We only need simple email for logging.
+	private SimpleEmail email;
+	//We will need to store smtp information for outgoing stuff.
+	private String smtpUser, smtpPass, smtpServer;
+	private int smtpPort;
+	
 	public Mailer() {
-		
+		email = new SimpleEmail();
+		//I'm hardcoding this port for now.
+		this.smtpPort = 587;
+	}
+	
+	public void setSMTP(String server, String user, String password) {
+		this.smtpServer = server;
+		this.smtpUser = user;
+		this.smtpPass = password;
+	}
+	
+	public void createAndSendSimpleEmail(String from, String to, String subject, String message) {
+		email.setHostName(this.smtpServer);
+		email.setSmtpPort(this.smtpPort);
+		email.setAuthenticator(new DefaultAuthenticator(this.smtpUser, this.smtpPass));
+		email.setTLS(true);
+		try {
+			email.setDebug(false);
+			email.setFrom(from);
+			email.addTo(to);
+			email.setSubject(subject);
+			email.setMsg(message);
+			System.out.println("Sending the email...");
+	        email.send();
+	        System.out.println("Your email has been sent!");
+		} catch (Exception e) {
+	        System.out.println("Your email has not been sent!");
+	        e.printStackTrace();
+	    }
 	}
 	
 }
--- src/com/rjh/javaexception/Main.java
+++ src/com/rjh/javaexception/Main.java
@@ -1,13 +1,42 @@
 package com.rjh.javaexception;
 
+import java.util.Scanner;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		
+		//testing the mailer
+		Mailer mailer = new Mailer();
+		
+		System.out.print("Server: ");
+		String server = in.nextLine();
+		
+		System.out.print("Username: ");
+		String username = in.nextLine();
+		
+		System.out.print("Password: ");
+		String password = in.nextLine();
+		
+		mailer.setSMTP(server, username, password);
+		
+		System.out.print("From: ");
+		String from = in.nextLine();
+		
+		System.out.print("To: ");
+		String to = in.nextLine();
+		
+		System.out.print("Subject: ");
+		String subject = in.nextLine();
+		
+		System.out.print("Message: ");
+		String message = in.nextLine();
+		
+		mailer.createAndSendSimpleEmail(from, to, subject, message);
 	}
 
 }
